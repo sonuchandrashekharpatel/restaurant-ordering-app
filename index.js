e2a9f2e587553153f72f3791cc042377d31c1225
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-04-09 21:00:46 +0530

    initial push of files

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+Solo Project - Restaurent Ordering App
+Requirements
+.   Follow the design spec
+.   Render the menu options using JavaScript
+.   Be able to add/remove items
+.   Have a payment modal with compulsory form inputs
+
+Stretch Goals
+.   Change the theme
+.   Offer a "meal deal" discount
+.   Allow users to rate theirs experience
+
+*/
